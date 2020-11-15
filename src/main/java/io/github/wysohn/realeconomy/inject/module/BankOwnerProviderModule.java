f892871d7adfc7f67b33a479913a8d8c35d7c999
wysohn <wysohn2002@naver.com>
2020-11-14 16:32:43 -0800

    provide empty set if no items are added

--- src/main/java/io/github/wysohn/realeconomy/inject/module/BankOwnerProviderModule.java
+++ src/main/java/io/github/wysohn/realeconomy/inject/module/BankOwnerProviderModule.java
@@ -1,7 +1,12 @@
 package io.github.wysohn.realeconomy.inject.module;
 
 import com.google.inject.AbstractModule;
+import com.google.inject.multibindings.Multibinder;
+import io.github.wysohn.realeconomy.interfaces.banking.IBankOwnerProvider;
 
 public class BankOwnerProviderModule extends AbstractModule {
-
+    @Override
+    protected void configure() {
+        Multibinder.newSetBinder(binder(), IBankOwnerProvider.class);
+    }
 }
--- src/main/java/io/github/wysohn/realeconomy/inject/module/BankUserProviderModule.java
+++ src/main/java/io/github/wysohn/realeconomy/inject/module/BankUserProviderModule.java
@@ -2,11 +2,17 @@ package io.github.wysohn.realeconomy.inject.module;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.Singleton;
+import com.google.inject.multibindings.Multibinder;
 import com.google.inject.multibindings.ProvidesIntoSet;
 import io.github.wysohn.realeconomy.interfaces.banking.IBankUserProvider;
 import io.github.wysohn.realeconomy.manager.user.UserManager;
 
 public class BankUserProviderModule extends AbstractModule {
+    @Override
+    protected void configure() {
+        Multibinder.newSetBinder(binder(), IBankUserProvider.class);
+    }
+
     @ProvidesIntoSet
     @Singleton
     public IBankUserProvider bankUserProvider(UserManager userManager){
