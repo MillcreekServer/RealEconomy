780a90c65c20d7f2d8b6b6233ab9d14813e1b88c
wysohn <wysohn2002@naver.com>
2020-11-21 14:10:08 -0800

    added missing namespace key

--- src/main/java/io/github/wysohn/realeconomy/inject/module/NamespacedKeyModule.java
+++ src/main/java/io/github/wysohn/realeconomy/inject/module/NamespacedKeyModule.java
@@ -2,6 +2,7 @@ package io.github.wysohn.realeconomy.inject.module;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
+import io.github.wysohn.realeconomy.inject.annotation.NamespaceKeyAssetSerialized;
 import io.github.wysohn.realeconomy.inject.annotation.NamespaceKeyCheckBalance;
 import io.github.wysohn.realeconomy.inject.annotation.NamespaceKeyCheckCurrency;
 import io.github.wysohn.realeconomy.main.RealEconomy;
@@ -10,6 +11,7 @@ import org.bukkit.NamespacedKey;
 public class NamespacedKeyModule extends AbstractModule {
     public static final String KEY_CHECK_CURRENCY = "RealEconomy_CheckCurrency";
     public static final String KEY_CHECK_BALANCE = "RealEconomy_CheckBalance";
+    public static final String KEY_ASSET_SERIALIZE = "RealEconomy_AssetSerialize";
 
     @Provides
     @NamespaceKeyCheckCurrency
@@ -21,5 +23,11 @@ public class NamespacedKeyModule extends AbstractModule {
     @NamespaceKeyCheckBalance
     NamespacedKey namespacedKeyBal(RealEconomy realEconomy) {
         return new NamespacedKey(realEconomy, KEY_CHECK_BALANCE);
+    }
+
+    @Provides
+    @NamespaceKeyAssetSerialized
+    NamespacedKey namedspacedKeyAssetSer(RealEconomy realEconomy){
+        return new NamespacedKey(realEconomy, KEY_ASSET_SERIALIZE);
     }
 }
