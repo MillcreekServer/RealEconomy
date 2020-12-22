e7fa897aa4ea659d0bc2a70bef76c2f10f6dc7b4
wysohn <wysohn2002@naver.com>
2020-12-21 19:22:28 -0800

    fix select queries to find the minimum offers properly

--- src/main/resources/select_sell_order_by_id.sql
+++ src/main/resources/select_sell_order_by_id.sql
@@ -1,4 +1,14 @@
 /*query by currency*/
-SELECT order_id, listing_uuid, category_id, timestamp, issuer, MIN(price) as price, currency_uuid, amount, maximum
-FROM sell_orders
-WHERE order_id=?;
+select order_id, listing_uuid, category_id, timestamp, issuer, price, currency_uuid, amount, maximum
+from sell_orders tbl1
+join (
+    -- Search the lowest price per listing_uuid
+    select listing_uuid as uuid, min(price) as p
+    from sell_orders
+    group by uuid
+    ) tbl2
+    on tbl1.price = tbl2.p
+where order_id=?
+-- since the sub-query may return multiple rows if there are multiple exact same priced items,
+-- choose the first one as output
+group by listing_uuid;
