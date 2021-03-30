a5dd0ce9d74420e2e626b7f45f7e9f31775755eb
wysohn <wysohn2002@naver.com>
2021-03-29 20:16:14 -0700

    prevent a player from manipulating price
    which isn't easy when there are lots of players, but usually there are very small amount of players exist in the server

--- src/main/resources/select_price_trend_highest.sql
+++ src/main/resources/select_price_trend_highest.sql
@@ -2,5 +2,6 @@ select * from trade_logs
 where DATEDIFF(CURDATE(), `timestamp`) < ?
     and currency_uuid = ?
     and listing_uuid = ?
+    and buyer != seller
 order by price desc
 LIMIT 1
--- src/main/resources/select_price_trend_lowest.sql
+++ src/main/resources/select_price_trend_lowest.sql
@@ -2,5 +2,6 @@ select * from trade_logs
 where DATEDIFF(CURDATE(), `timestamp`) < ? -- last N days
     and currency_uuid = ? -- target currency
     and listing_uuid = ? -- target listing
+    and buyer != seller
 order by price asc
 LIMIT 1
