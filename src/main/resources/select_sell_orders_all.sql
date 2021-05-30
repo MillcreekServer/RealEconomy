d32a8e8e541b3c884c35b80348dcc397c536e2ca
wysohn <wysohn2002@naver.com>
2021-05-29 22:37:28 -0700

    fixed min price query

--- src/main/resources/select_sell_orders_all.sql
+++ src/main/resources/select_sell_orders_all.sql
@@ -1,14 +1,14 @@
 /*query by currency*/
-select order_id, ln2.listing_uuid, category_id, timestamp, issuer, price, currency_uuid, amount, maximum
+select order_id, ln2.listing_uuid, category_id, timestamp, issuer, tbl2.price, currency_uuid, amount, maximum
 from sell_orders tbl1 left join listing_names ln2
 on tbl1.listing_uuid = ln2.listing_uuid
 join (
     -- Search the lowest price per listing_uuid
-    select listing_uuid as uuid, min(price) as p
+    select order_id as id, listing_uuid as uuid, min(price) as price
     from sell_orders
     group by uuid
     ) tbl2
-    on tbl1.price = tbl2.p
+    on tbl1.order_id = tbl2.id
 -- since the sub-query may return multiple rows if there are multiple exact same priced items,
 -- choose the first one as output
 group by listing_uuid
