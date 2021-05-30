3206cef3420f6290d4ebd1562e982332ce280a71
wysohn <wysohn2002@naver.com>
2021-05-29 22:16:16 -0700

    sort by names

--- src/main/resources/select_sell_orders.sql
+++ src/main/resources/select_sell_orders.sql
@@ -1,6 +1,7 @@
 /*query by currency*/
-select order_id, listing_uuid, category_id, timestamp, issuer, price, currency_uuid, amount, maximum
-from sell_orders tbl1
+select order_id, ln2.listing_uuid, category_id, timestamp, issuer, price, currency_uuid, amount, maximum
+from sell_orders tbl1 left join listing_names ln2
+on tbl1.listing_uuid = ln2.listing_uuid
 join (
     -- Search the lowest price per listing_uuid
     select listing_uuid as uuid, min(price) as p
@@ -12,4 +13,5 @@ where category_id = ?
 -- since the sub-query may return multiple rows if there are multiple exact same priced items,
 -- choose the first one as output
 group by listing_uuid
+order by REVERSE(ln2.name)
 LIMIT ?, ?;
